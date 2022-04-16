a5df2f1b90999dd7ba93f663d923e9f7dc562a31
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-04-15 19:27:54 -0500

    wip

--- src/clients/tartatusClient.ts
+++
@@ -1,3 +0,0 @@
-// Client for tartarus service
-// DTOs/models should be shared between service and client
-// Client is leveraged in CLI tool
--- src/tools/thanatos/src/thanatos.ts
+++
@@ -1,5 +0,0 @@
-// Should provide a CLI interface that will do the following:
-// 1) Tail a file and forward to the tartarus service
-// 2) Open a TCP socket server and multiplex to tartarus service
-// 3) Allow connecting to a TCP socket serevr and proxy to tartarus service
-// 4) Create a pipe and multiplex to tartarus service
