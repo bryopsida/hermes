1324055297e3c0843fa5d3732fa7d851b03b733c
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-04-16 12:51:52 -0500

    more comments outlining thoughts on watches and classifiers

--- src/services/classificationManager/classificationService.ts
+++ src/services/classificationManager/classificationService.ts
@@ -1,5 +1,17 @@
 import { IService } from '../../common/interfaces/service'
 
+// What does a classifier do?
+// It isn't an alert condition, instead it's a query
+// that looks at the data and extracts a value or values from it.
+// it then updates metadata in the object. So what do you need to define a classifier?
+// 1) A name
+// 2) Unique ID
+// 3) A query
+// 4) A query type (graphql)
+// 5) A destination path
+// 6) A destination type
+//
+
 // TODO
 export class ClassificationService implements IService {
   public static readonly NAME : string = 'classification_manager'
