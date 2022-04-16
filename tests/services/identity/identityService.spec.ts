edf20cb59260052330fdccf30bb45b0a42ebdeea
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-04-16 14:40:10 -0500

    fix test build

--- tests/services/identity/identityService.spec.ts
+++ tests/services/identity/identityService.spec.ts
@@ -1,35 +1,43 @@
 import { IdentityService } from '../../../src/services/identity/identityService'
+import { fastify } from 'fastify'
+
 /* eslint-disable no-undef */
 describe('Services.Identity', () => {
   it('Can Determine If It Can Serve Traffic', async () => {
-    const identityService = new IdentityService()
+    const app = fastify()
+    const identityService = new IdentityService(app)
     const canServe = await identityService.canServeTraffic()
     expect(canServe).toBe(true)
   })
   it('Can Start', async () => {
-    const identityService = new IdentityService()
+    const app = fastify()
+    const identityService = new IdentityService(app)
     await identityService.start()
     expect(await identityService.isAlive).toBe(true)
   })
   it('Can Stop', async () => {
-    const identityService = new IdentityService()
+    const app = fastify()
+    const identityService = new IdentityService(app)
     await identityService.start()
     await identityService.stop()
     expect(await identityService.isAlive).toBe(false)
   })
   it('Serves Traffic', async () => {
-    const identityService = new IdentityService()
+    const app = fastify()
+    const identityService = new IdentityService(app)
     const serveTraffic = identityService.servesTraffic()
     expect(serveTraffic).toBe(true)
   })
   it('Can Be Destroyed', async () => {
-    const identityService = new IdentityService()
+    const app = fastify()
+    const identityService = new IdentityService(app)
     await identityService.start()
     await identityService.destroy()
     expect(await identityService.isAlive).toBe(false)
   })
   it('Can Determine Its Alive', async () => {
-    const identityService = new IdentityService()
+    const app = fastify()
+    const identityService = new IdentityService(app)
     expect(identityService.isAlive()).resolves.toBe(true)
   })
 })
--- tests/services/user/userService.spec.ts
+++ tests/services/user/userService.spec.ts
@@ -1,35 +1,42 @@
 import { UserService } from '../../../src/services/user/userService'
+import { fastify } from 'fastify'
 
 /* eslint-disable no-undef */
 describe('Services.User', () => {
   it('Can Determine If It Can Serve Traffic', async () => {
-    const userService = new UserService()
+    const app = fastify()
+    const userService = new UserService(app)
     const canServe = await userService.canServeTraffic()
     expect(canServe).toBe(true)
   })
   it('Can Start', async () => {
-    const userService = new UserService()
+    const app = fastify()
+    const userService = new UserService(app)
     await userService.start()
     expect(userService.isAlive()).resolves.toBe(true)
   })
   it('Can Stop', async () => {
-    const userService = new UserService()
+    const app = fastify()
+    const userService = new UserService(app)
     await userService.start()
     await userService.stop()
     expect(userService.isAlive()).resolves.toBe(false)
   })
   it('Serves Traffic', async () => {
-    const userService = new UserService()
+    const app = fastify()
+    const userService = new UserService(app)
     expect(userService.servesTraffic()).toBe(true)
   })
   it('Can Be Destroyed', async () => {
-    const userService = new UserService()
+    const app = fastify()
+    const userService = new UserService(app)
     await userService.start()
     await userService.destroy()
     expect(userService.isAlive()).resolves.toBe(false)
   })
   it('Can Determine Its Alive', async () => {
-    const userService = new UserService()
+    const app = fastify()
+    const userService = new UserService(app)
     expect(userService.isAlive()).resolves.toBe(true)
   })
 })
