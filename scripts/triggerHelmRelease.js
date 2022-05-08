665a47f53821c2cfa6717087bceaa42d2e1503af
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-05-08 13:38:36 -0500

    set repo name directly in script

--- scripts/triggerHelmRelease.js
+++ scripts/triggerHelmRelease.js
@@ -1,13 +1,12 @@
 const { Octokit } = require('@octokit/rest')
 const token = process.env.GITHUB_TOKEN
-const repo = process.env.GITHUB_REPOSITORY
 const owner = process.env.GITHUB_REPOSITORY_OWNER
 const octokit = new Octokit({
   auth: token
 })
 octokit.rest.repos.createDispatchEvent({
   owner: owner,
-  repo: repo,
+  repo: 'hermes',
   event_type: 'helm-release',
   client_payload: {}
 }).then(() => {
