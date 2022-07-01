9f239a51a7b7a7ceddc4e8bcfffba7a59d33d6d9
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-07-01 17:56:59 -0500

    stub test cases for crypto classes

---
+++ tests/common/crypto/crypto.spec.ts
@@ -0,0 +1,28 @@
+
+/* eslint-disable no-undef */
+describe('Crypto', () => {
+  it('can generate a DEK', async () => {
+    expect(true).toBe(false)
+  })
+  it('can generate a root key', async () => {
+    expect(true).toBe(false)
+  })
+  it('can encrypt a buffer', async () => {
+    expect(true).toBe(false)
+  })
+  it('can encrypt a stream', async () => {
+    expect(true).toBe(false)
+  })
+  it('can encrypt a string', async () => {
+    expect(true).toBe(false)
+  })
+  it('can decrypt a buffer', async () => {
+    expect(true).toBe(false)
+  })
+  it('can decrypt a stream', async () => {
+    expect(true).toBe(false)
+  })
+  it('can decrypt a string', async () => {
+    expect(true).toBe(false)
+  })
+})
---
+++ tests/common/crypto/mongoKeyStore.spec.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+describe('MongoKeyStore', () => {
+  it('can manage a DEK', async () => {
+    expect(true).toBe(false)
+  })
+  it('can manage a root key', async () => {
+    expect(true).toBe(false)
+  })
+  it('can shred all the keys', async () => {
+    expect(true).toBe(false)
+  })
+  it('fails when host seed changes', async () => {
+    expect(true).toBe(false)
+  })
+  it('fails when host key changes', async () => {
+    expect(true).toBe(false)
+  })
+})
