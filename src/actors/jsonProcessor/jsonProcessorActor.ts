2d83a52431c8bf29c3270f042e7bf4732059d1f8
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-07-24 20:43:22 -0500

    outline next steps for classifiers in tasks

--- src/actors/jsonProcessor/jsonProcessorActor.ts
+++ src/actors/jsonProcessor/jsonProcessorActor.ts
@@ -19,6 +19,8 @@ export class JsonProcessorActor extends KafkaConsumerActor<IUnprocesseedJsonData
   readonly topic: string
   kafkaConsumer?: KafkaConsumer
 
+  // TODO: pipe in bull client to create and get task results
+  // TODO: pipe in classification api client to get classifications
   constructor (config : IActorConfig) {
     super()
     this.config = config
@@ -27,8 +29,15 @@ export class JsonProcessorActor extends KafkaConsumerActor<IUnprocesseedJsonData
     this.log.info(`${this.name} actor created`)
   }
 
+  // TODO: ensure upstream is including IDataSource of the triggerring source in the message
   actOn (message: IUnprocesseedJsonData): Promise<IProcessedJsonData> {
     this.log.debug('json processor actor acting on message')
+    // TODO: add loop to fetch all classifications
+    // TODO: for each classification if any, kick off task in bull to compute metadata
+    // TODO: await each task to complete
+    // TODO: merge all metadata into one object, use resultBucketName to store it
+    // TODO: return result
+    // TODO: ensure returned result goes to next topic for watch actor
     return Promise.resolve({} as IProcessedJsonData)
   }
 }
