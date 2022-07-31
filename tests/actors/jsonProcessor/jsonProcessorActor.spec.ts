2332872fc4d8f9fad0ba416edf85419df633b698
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-07-31 14:09:06 -0500

    stub out actor tests

---
+++ tests/actors/jsonProcessor/jsonProcessorActor.spec.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+describe('jsonProcessorActor', () => {
+  beforeAll(() => {
+    // spin up kafka and zookeeper
+  })
+  afterAll(() => {
+    // clean up test containers
+  })
+  it('Should transform incoming messages and publish result to destination topic', async () => {
+    expect(false).toBeTruthy()
+  })
+  it('Should cleanup resources when torn down', async () => {
+    expect(false).toBeTruthy()
+  })
+})
